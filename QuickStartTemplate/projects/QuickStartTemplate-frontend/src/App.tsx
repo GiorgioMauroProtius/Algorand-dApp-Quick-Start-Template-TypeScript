457434d3ad8518bf8df31ae17db0c832333b38a7
Giorgio Mauro <giorgiomauro63@gmail.com>
2025-11-14 20:04:43 +0100

    Update App.tsx

--- QuickStartTemplate/projects/QuickStartTemplate-frontend/src/App.tsx
+++ QuickStartTemplate/projects/QuickStartTemplate-frontend/src/App.tsx
@@ -1,21 +1,66 @@
-import { useWallet } from "@txnlab/use-wallet";
+import {
+  SupportedWallet,
+  WalletId,
+  WalletManager,
+  WalletProvider,
+} from "@txnlab/use-wallet-react";
+import { SnackbarProvider } from "notistack";
+import Home from "./Home";
+import {
+  getAlgodConfigFromViteEnvironment,
+  getKmdConfigFromViteEnvironment,
+} from "./utils/network/getAlgoClientConfigs";
+import { Analytics } from "@vercel/analytics/react";
 
-export function App() {
-  const { activeAddress, signTransactions, sendTransactions } = useWallet();
+let supportedWallets: SupportedWallet[];
 
-  return (
-    <div>
-      <h1>Protius Demo</h1>
-
-      {!activeAddress ? (
-        <button onClick={() => window.openWalletConnectModal()}>
-          Connect Wallet
-        </button>
-      ) : (
-        <p>Connected: {activeAddress}</p>
-      )}
-    </div>
-  );
+if (import.meta.env.VITE_ALGOD_NETWORK === "localnet") {
+  const kmdConfig = getKmdConfigFromViteEnvironment();
+  supportedWallets = [
+    {
+      id: WalletId.KMD,
+      options: {
+        baseServer: kmdConfig.server,
+        token: String(kmdConfig.token),
+        port: String(kmdConfig.port),
+      },
+    },
+  ];
+} else {
+  supportedWallets = [
+    { id: WalletId.DEFLY },
+    { id: WalletId.PERA },
+    { id: WalletId.EXODUS },
+    // WalletConnect v2 etc can be added later if needed
+  ];
 }
 
-export default App;
+export default function App() {
+  const algodConfig = getAlgodConfigFromViteEnvironment();
+
+  const walletManager = new WalletManager({
+    wallets: supportedWallets,
+    defaultNetwork: algodConfig.network,
+    networks: {
+      [algodConfig.network]: {
+        algod: {
+          baseServer: algodConfig.server,
+          port: algodConfig.port,
+          token: String(algodConfig.token),
+        },
+      },
+    },
+    options: {
+      resetNetwork: true,
+    },
+  });
+
+  return (
+    <WalletProvider manager={walletManager}>
+      <SnackbarProvider maxSnack={3}>
+        <Home />
+        <Analytics />
+      </SnackbarProvider>
+    </WalletProvider>
+  );
+}
