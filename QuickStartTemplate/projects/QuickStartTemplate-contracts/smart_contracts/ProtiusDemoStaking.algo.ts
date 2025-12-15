294ec3369d695df87e72ae9777841be866e26b46
Giorgio Mauro <giorgiomauro63@gmail.com>
2025-12-15 17:46:45 +0100

    Update import statement for tealscript module

--- QuickStartTemplate/projects/QuickStartTemplate-contracts/smart_contracts/ProtiusDemoStaking.algo.ts
+++ QuickStartTemplate/projects/QuickStartTemplate-contracts/smart_contracts/ProtiusDemoStaking.algo.ts
@@ -9,7 +9,7 @@ import {
   Bytes,
   Uint64,
   Global,
-} from "@algorandfoundation/tealscript";
+} from "@algorandfoundation/algokit-utils/types/tealscript";
 
 /**
  * ============================
